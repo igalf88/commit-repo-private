80cb10017eb8ff2aed634aff9e3ef37f7daa5ee4
igalf88 <102952932+igalf88@users.noreply.github.com>
2023-01-09 15:13:16 +0200

    Add files via upload

---
+++ rds/variables.tf
@@ -0,0 +1,16 @@
+
+variable "db_instance" {
+  default = "db.t2.micro"
+}
+
+variable "rds_subnet1" {
+  default = "subnet-0fc1648c872b256dd"
+}
+
+variable "rds_subnet2" {
+  default = "subnet-0676277f284ca43ba"
+}
+
+variable "vpc_id" {
+  default = "vpc-0dfff3b2275b9da80"
+}
