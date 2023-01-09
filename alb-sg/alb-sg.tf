a19c4fae099d6abd5726e698a7b98cf5a4159c1d
igalf88 <102952932+igalf88@users.noreply.github.com>
2023-01-09 15:12:23 +0200

    Add files via upload

---
+++ alb-sg/alb-sg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = var.aws_vpc
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [var.aws_vpc_cidr]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
---
+++ alb-sg/var.tf
@@ -0,0 +1,9 @@
+variable "aws_vpc" {
+  type = string
+  default = "vpc-0dfff3b2275b9da80"
+}
+
+variable "aws_vpc_cidr" {
+  type = string
+  default = "10.0.0.0/16"
+}
